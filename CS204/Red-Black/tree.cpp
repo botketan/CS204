8c770d384c2b7c19ace47e627033396ad2d684f1
botketan <95174279+botketan@users.noreply.github.com>
2022-11-20 17:01:18 +0530

    added delete

--- CS204/Red-Black/tree.cpp
+++ CS204/Red-Black/tree.cpp
@@ -22,6 +22,17 @@ class node
     }
 };
 
+struct node* root = NULL;
+
+int color(struct node*p)
+{
+    if (p== NULL)
+    return 0;
+    else
+    return p->color;
+}
+
+
 void infix(struct node*p)
 {
     if(p==NULL)
@@ -33,37 +44,70 @@ void infix(struct node*p)
 
 void Leftrotate(struct node*p)
 {
-    struct node* temp = p->parent;
-    p->parent = temp->parent;
-    temp->leftChild = p->rightChild;
-    p->rightChild = temp;
-    if(p->parent->leftChild==temp)
-    p->parent->leftChild = p;
-    else
-    p->parent->rightChild = p;
+    struct node* pa = p->parent;
+    struct node * g = p->parent->parent;
+    if(g!=NULL)
+    {
+        if(pa == g->leftChild)
+        g->leftChild = p;
+        else
+        g->rightChild =p;
+    }
+    p->parent = g;
+    pa->parent =p;
+    if(p->leftChild!=NULL)
+    p->leftChild->parent = pa; 
+    pa->rightChild = p->leftChild;
+    p->leftChild = pa;
+
+    if(g==NULL)
+    root = p;
 }
 
 void Rightrotate(struct node*p)
 {
-    struct node* temp = p->parent;
-    p->parent = temp->parent;
-    temp->rightChild = p->leftChild;
-    p->leftChild = temp;
-    if(p->parent->leftChild==temp)
-    p->parent->leftChild = p;
-    else
-    p->parent->rightChild = p;
+    struct node* pa = p->parent;
+    struct node * g = p->parent->parent;
+    if(g!=NULL)
+    {
+        if(pa == g->leftChild)
+        g->leftChild = p;
+        else
+        g->rightChild =p;
+    }
+    p->parent = g;
+    pa->parent =p;
+    if(p->rightChild!=NULL)
+    p->rightChild->parent = pa; 
+    pa->leftChild = p->rightChild;
+    p->rightChild = pa;
+
+    if(g==NULL)
+    root = p;
 }
+
+struct node * search(struct node*p,int x)
+{
+    if(p==NULL)
+    return NULL;
+    if(x==p->val)
+    return p;
+    if(x>p->val)
+    search(p->rightChild,x);
+    else
+    search(p->leftChild,x);
+}
+
 
 void insertCheck(struct node* p)
 {
     if(p->parent==NULL)
     {
-        if(p->color=1)
+        if(p->color==1)
         p->flipcolor();
         return;
     }
-    if(p->parent->color=0)
+    if(p->parent->color==0)
     return;
     else
     {
@@ -78,7 +122,7 @@ void insertCheck(struct node* p)
             {
                 Leftrotate(p);
             }
-            if(g->rightChild->color=1)
+            if(color(g->rightChild)==1)
             {
                 pa->flipcolor();
                 g->flipcolor();
@@ -97,13 +141,18 @@ void insertCheck(struct node* p)
             if(p==pa->leftChild)
             {
                 Rightrotate(p);
+                pa= p;
+                p = p->rightChild;
             }
-            if(g->leftChild->color=1)
+            if(g->leftChild!=NULL)
             {
-                pa->flipcolor();
-                g->flipcolor();
-                g->leftChild->flipcolor();
-                insertCheck(g);
+                if(color(g->leftChild)==1)
+                {
+                    pa->flipcolor();
+                    g->flipcolor();
+                    g->leftChild->flipcolor();
+                    insertCheck(g);
+                }
             }
             else
             {
@@ -116,42 +165,166 @@ void insertCheck(struct node* p)
 
 }
 
-void insert( struct node**root,int v)
+
+
+void insert( struct node*p,int v)
 {
-    cout<<"inserting \n";
-    struct node *p = *root;
+    node* obj = new node(v,1);
     while(1)
     {
         if(v>p->val)
         {
             if(p->rightChild==NULL)
-            break;
+            {
+                obj->parent = p;
+                p->rightChild = obj;
+                break;
+            }
+            else
             p = p->rightChild;
         }
-        
         else
         {
             if(p->leftChild==NULL)
-            break;
+            {
+                obj->parent = p;
+                p->leftChild = obj;
+                break;
+            }
+            else
             p = p->leftChild;
         }
-        
     }
-    node* obj = new node(v,1);
-    obj->parent = p;
-    if(v>p->val)
-    p->rightChild=obj;
-    else
-    p->leftChild = obj;
+    cout<<"Inserted\n";
     insertCheck(obj);
 }
 
+void check(struct node *p)
+{
+    if(p->parent==NULL)
+    {
+        if(p->color==1)
+        p->flipcolor();
+        return;
+    }
+    if(p->color==1)
+    {
+        p->flipcolor();
+        return;
+    }
+    if(p==p->parent->leftChild)
+    {
+        struct node * y = p->parent->rightChild;
+        if(y->color==1)
+        {
+            y->parent->flipcolor();
+            Leftrotate(y);
+            check(p);
+        }
+        else
+        {
+            if(color(y->leftChild)==0 && color(y->rightChild)==0)
+            {
+                y->flipcolor();
+                check(y->parent);
+            }
+            else if (color(y->leftChild)==1 && color(y->rightChild)==0)
+            {
+                y->flipcolor();
+                y->leftChild->flipcolor();
+                Rightrotate(y->leftChild);
+                check(p);
+            }
+            else if(color(y->rightChild)==1)
+            {
+                y->color = p->parent->color;
+                p->parent->color =0;
+                y->rightChild->flipcolor();
+                Rightrotate(y);
+            }
+        }
+    }
+    else
+    {
+        struct node * y = p->parent->leftChild;
+        if(color(y)==1)
+        {
+            y->parent->flipcolor();
+            Rightrotate(y);
+            check(p);
+        }
+        else
+        {
+            if(color(y->leftChild)==0 && color(y->rightChild)==0)
+            {
+                y->flipcolor();
+                check(y->parent);
+            }
+            else if (color(y->leftChild)==0 && color(y->rightChild)==1)
+            {
+                y->flipcolor();
+                y->rightChild->flipcolor();
+                Leftrotate(y->leftChild);
+                check(p);
+            }
+            else if(color(y->leftChild)==1)
+            {
+                y->color = p->parent->color;
+                p->parent->color =0;
+                y->leftChild->flipcolor();
+                Leftrotate(y);
+            }
+        }
+    }
+}
 
+
+void del(struct node *p)
+{
+    
+    if(p == NULL)
+    {
+        cout<<"No such element"<<endl;
+        return;
+    }
+    if(p->leftChild!=NULL && p->rightChild!=NULL)
+    {
+        struct node *pt = p->leftChild;
+        while(pt->rightChild!=NULL)
+        {
+            pt= pt->rightChild;
+        }
+        swap(pt->val,p->val);
+        del(pt);
+    }
+    else
+    {
+        if(p->leftChild!=NULL)
+        {
+            if(p->parent->leftChild==p)
+            p->parent->leftChild = p->leftChild;
+            else
+            p->parent->rightChild = p->leftChild;
+            p->leftChild->parent = p->parent;
+        }
+        else
+        {
+            if(p->parent->leftChild==p)
+            p->parent->leftChild = p->rightChild;
+            else
+            p->parent->rightChild = p->rightChild;
+            if(p->rightChild!=NULL)
+            p->rightChild->parent = p->parent;
+        }
+        check(p->parent);
+        free(p);
+    }
+}
 
 
 int main()
 {
-    struct node* root = NULL;
+
     int n;
     cin>>n;
     int a[n];
@@ -168,8 +341,11 @@ int main()
             root = obj;
         }
         else
-        insert(&root,a[i]);
-        cout<<i;
+        insert(root,a[i]);
+        cout<<i<<endl;
     }
+    infix(root);
+    cin>>n;
+    del(search(root,n));
     infix(root);
 }
